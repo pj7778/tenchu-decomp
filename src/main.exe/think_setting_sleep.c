3e9f8a339f5614282b8fe7a4050e472e31900558
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2023-05-16 23:49:32 +0900

    matching think_setting_sleep?

--- src/main.exe/think_setting_sleep.c
+++ src/main.exe/think_setting_sleep.c
@@ -1,28 +1,27 @@
 #include "common.h"
 #include "main.exe.h"
 
-INCLUDE_ASM("config/../.shake/gen/main.exe/asm/nonmatchings/think_setting_sleep", think_setting_sleep_);
-/*
+// INCLUDE_ASM("config/../.shake/gen/main.exe/asm/nonmatchings/think_setting_sleep", think_setting_sleep_);
+
 s32 think_setting_sleep_(void)
 {
+    something_about_current_animation *temp_a0;
+    s32 var_a1;
+    s32 var_v0;
 
-    s32 iVar1;
-    u16 uVar2;
-
-    uVar2 = 0;
-    if (CHARACTER_BEING_UPDATED_->something_about_current_animation->animation_state_perhaps == 0x100)
+    temp_a0 = &CHARACTER_BEING_UPDATED_->something_about_current_animation;
+    var_a1 = 0;
+    if (temp_a0->animation_state_perhaps == 0x100)
     {
         ALERT_STATUS_ = -1;
     }
-    else if (CHARACTER_BEING_UPDATED_->something_about_current_animation->frames_since_animation_start == 0)
+    else if (temp_a0->frames_since_animation_start == 0)
     {
-        uVar2 = 0x1001;
+        var_a1 = 0x1001;
     }
-    if ((FRAMES_UNTIL_END_OF_ALERT != 0) || (((s32)ACTUALLY_ALERT_STATUS_ & 0x8000U) != 0))
+    if ((FRAMES_UNTIL_END_OF_ALERT != 0) || (var_v0 = var_a1 << 0x10, ((ACTUALLY_ALERT_STATUS_ & 0x8000) != 0)))
     {
-        iVar1 = turn_towards_player_(0, 0);
-        uVar2 = (u16)iVar1 & 0xa000;
+        var_v0 = (turn_towards_player_(0, 0) & 0xA000) << 0x10;
     }
-    return (s32)(s16)uVar2;
+    return var_v0 >> 0x10;
 }
-*/
